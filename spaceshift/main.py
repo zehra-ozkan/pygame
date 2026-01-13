80377f722132bb49291b60087f847671ac04034d
zehra-ozkan <zehra.ozkan138@gmail.com>
2026-01-13 21:17:54 +0300

    dvd exercise

--- spaceshift/main.py
+++ spaceshift/main.py
@@ -11,6 +11,8 @@ display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("this is the caption")
 exit = False
 
+clock = pygame.time.Clock()
+
 #surface
 
 surf = pygame.Surface((100, 200))
@@ -29,6 +31,9 @@ player_surf  = pygame.image.load(path).convert_alpha()
 player_rect = player_surf.get_frect(center=(WIDTH / 2, HEIGHT / 2))
 
 star_surface = pygame.image.load(join('spaceshift','images', 'star.png')).convert_alpha()
+star_coordinates = []
+for i in range (20):
+    star_coordinates.append((randrange(0, WIDTH), randrange(0, HEIGHT)))
 
 meteor_surface = pygame.image.load(join('spaceshift', 'images', 'meteor.png')).convert_alpha()
 meteor_rectangle = meteor_surface.get_frect(center=(WIDTH / 2, HEIGHT/2))
@@ -36,13 +41,14 @@ meteor_rectangle = meteor_surface.get_frect(center=(WIDTH / 2, HEIGHT/2))
 laser_surface = pygame.image.load(join('spaceshift', 'images', 'laser.png')).convert_alpha()
 laser_rectangle = laser_surface.get_frect(bottomleft=(20, HEIGHT - 20))
 
-star_coordinates = []
-for i in range (20):
-    star_coordinates.append((randrange(0, WIDTH), randrange(0, HEIGHT)))
 
 
-dir = 0.5
+dir = pygame.math.Vector2(2, -2)
+speed = 60
+
 while not exit:
+    dt = clock.tick(60) / 1000 #delta time measrures hte time took by the computer to render in between the last 2 frames, how much time has changed since the last 2 frames, different in everty computer
+    print("dt",dt)
     for event in pygame.event.get(): #this gets all the events in the pygame
         if event.type == pygame.QUIT:
             exit = True
@@ -55,27 +61,30 @@ while not exit:
     #draw the game
     #surface_x = surface_x + 0.1
     #order of the drawing matters!! background first otherwise nothing will be seen
-    display_surface.fill('darkgrey')#if you remove htis line it does not claer
     
     for i in star_coordinates:
         display_surface.blit(star_surface, i)#putting one surface on another surface
     
-    display_surface.blit(meteor_surface, meteor_rectangle)    
-    display_surface.blit(laser_surface, laser_rectangle)    
+    
     #display_surface.blit(player_surf, (surface_x, 5))#putting one surface on another surface
     
     #bounce from left to right
-    if player_rect.right >= WIDTH:
-        dir = -0.5
-    if player_rect.left <=0:
-        print("come herdddddddddddddddddddddddddddddddddddde")
-        dir = 0.5
+    if player_rect.right >= WIDTH or player_rect.left <=0:
+        dir.x = dir.x * -1
         
-    print(dir)
-    player_rect.right = player_rect.right + dir
+    if player_rect.bottom >= HEIGHT or player_rect.top <=0:
+        dir.y = dir.y * -1
+        
+    #print(dir)
     
+    #print(player_rect.center)
+    player_rect.center = player_rect.center + dir * speed * dt
+    #print(player_rect.center)
+    
+    display_surface.fill('darkgrey')#if you remove htis line it does not claer
+    display_surface.blit(meteor_surface, meteor_rectangle)    
+    display_surface.blit(laser_surface, laser_rectangle)    
     display_surface.blit(player_surf, player_rect)#putting one surface on another surface
-    player_rect.bottom = 200
     
     pygame.display.update()
 
