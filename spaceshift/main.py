8a4bbf45e8ba081f55dda865a82866ffadec7724
zehra-ozkan <zehra.ozkan138@gmail.com>
2026-01-13 19:12:57 +0300

    exercise 2

--- spaceshift/main.py
+++ spaceshift/main.py
@@ -1,5 +1,7 @@
 import pygame
 from os.path import join
+from random import randrange
+
 
 pygame.init()
 
@@ -24,6 +26,10 @@ print(path)
 player_surf  = pygame.image.load(path).convert_alpha()
 #player_surf  = pygame.image.load('./spaceshift/images/player.png')# this might not have worked in macos where it expects \
 
+star_surface = pygame.image.load(join('spaceshift','images', 'star.png')).convert_alpha()
+star_coordinates = []
+for i in range (20):
+    star_coordinates.append((randrange(0, WIDTH), randrange(0, HEIGHT)))
 
 while not exit:
     for event in pygame.event.get(): #this gets all the events in the pygame
@@ -37,6 +43,12 @@ while not exit:
     #draw the game
     #surface_x = surface_x + 0.1
     display_surface.fill('darkgrey')#if you remove htis line it does not claer
+    
+    for i in star_coordinates:
+        print(i)
+        display_surface.blit(star_surface, i)#putting one surface on another surface
+        
+    
     display_surface.blit(player_surf, (surface_x, 5))#putting one surface on another surface
     pygame.display.update()
 
