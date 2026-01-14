dd0242253d81cb717cf708cc3b8e5bed221151ab
zehra-ozkan <zehra.ozkan138@gmail.com>
2026-01-14 22:35:53 +0300

    rotation

--- spaceshift/main.py
+++ spaceshift/main.py
@@ -62,12 +62,15 @@ class Meteor(pygame.sprite.Sprite):
     def __init__(self, surf, groups):
         super().__init__(groups)
         self.image = surf
+        self.original_image = surf
+        self.rotation_angle = 0
+        self.rotation_speed = randrange(-120 , 150)
         self.rect = self.image.get_frect(midbottom=(randrange(0 , WIDTH), 0))
         self.speed = randrange(250, 550)
         self.live = True
         self.birth = pygame.time.get_ticks()
         self.lifetime = 3000 #this is two seconds
-        self.direction = pygame.Vector2(uniform(-0.5 , 0.5) , 1)#so that the meteors do not move in a straight line
+        self.direction = pygame.Vector2(uniform(-0.8 , 0.8) , 1)#so that the meteors do not move in a straight line
         
     def meteor_timer(self):
         if not self.live:
@@ -76,14 +79,24 @@ class Meteor(pygame.sprite.Sprite):
             self.live = False
     def update(self, dt):
         #movement of a meteor
-        
+        self.rotation_angle = (self.rotation_angle + self.rotation_speed * dt ) % 360
         self.rect.center = self.rect.center + self.speed * self.direction.normalize() * dt
-        self.direction.x = uniform(-0.5 , 0.5) if randrange(0 , 10) > 7 else self.direction.x
+        self.image = pygame.transform.rotozoom(self.original_image, self.rotation_angle, 1)
+        self.rect = self.image.get_frect(center=self.rect.center)
+        self.direction.x = uniform(-0.4 , 0.4) if randrange(0 , 10) > 7 else self.direction.x
         self.meteor_timer()
         if not self.live:
             self.kill()    
         
-        
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000) + 1
+    text_surf = font.render(str(current_time)  , True, (201 , 170, 250))
+    text_rect = text_surf.get_frect(midbottom=(WIDTH / 2, HEIGHT - 20))
+    
+    pygame.draw.rect(display_surface, (150, 150 , 250), text_rect.inflate(20,16).move(0 , -8) , 5, 5)
+    display_surface.blit(text_surf, text_rect)
+    
+     
 pygame.init()
 
 WIDTH, HEIGHT = 1280, 720
@@ -94,7 +107,6 @@ exit = False
 
 clock = pygame.time.Clock()
 
-#surface
 
 
 
@@ -107,17 +119,16 @@ for i in range (20):
     star = Star(all_sprites, star_surface)
 
 player = Player(all_sprites)
-
 meteor_surface = pygame.image.load(join('spaceshift', 'images', 'meteor.png')).convert_alpha()
-
 laser_surface = pygame.image.load(join('spaceshift', 'images', 'laser.png')).convert_alpha()
+font = pygame.font.Font(join('spaceshift', 'images', 'font.ttf'), 30)
 
 #custom event -> meteor event
 meteor_event = pygame.event.custom_type()
 pygame.time.set_timer(meteor_event, 500) #it will be triggered every 500 miliseconds, twice in a second
 
 while not exit:
-    delta_time = clock.tick(120) / 1000 #delta time measrures hte time took by the computer to render in between the last 2 frames, how much time has changed since the last 2 frames, different in everty computer
+    delta_time = clock.tick(60) / 1000 #delta time measrures hte time took by the computer to render in between the last 2 frames, how much time has changed since the last 2 frames, different in everty computer
     for event in pygame.event.get(): #this gets all the events in the pygame
         if event.type == pygame.QUIT:
             exit = True
@@ -128,12 +139,13 @@ while not exit:
     #update
     all_sprites.update(delta_time)
     collided_sprites = pygame.sprite.groupcollide(laser_sprites, meteor_sprites, True, True)
-    
+    if pygame.sprite.spritecollide(player, meteor_sprites, True, pygame.sprite.collide_mask):
+        exit = True
 
-    display_surface.fill('darkgrey')#if you remove htis line it does not claer
+    display_surface.fill('#3a2e3f')#if you remove htis line it does not claer
     all_sprites.draw(display_surface)
+    display_score()
     
-    #collision test
     
     pygame.display.update()
 
