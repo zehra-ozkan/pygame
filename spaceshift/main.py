8e8b412c3f9da3ea876494a4e661d4abdd3f7628
zehra-ozkan <zehra.ozkan138@gmail.com>
2026-01-14 14:53:32 +0300

    star exercise completed

--- spaceshift/main.py
+++ spaceshift/main.py
@@ -20,7 +20,16 @@ class Player(pygame.sprite.Sprite): #inheriting from sprite sprite is a class wi
         self.direction = self.direction.normalize() if self.direction.magnitude() > 0 else self.direction #this stabilises teh speed
         self.rect.center = self.rect.center +  self.direction * self.speed * dt #multipllying with dt makes the movement speed independent from fps
 
-        
+            #laser
+        k = pygame.key.get_just_pressed()
+        if k[pygame.K_SPACE]:
+            print("space pressed")
+
+class Star(pygame.sprite.Sprite):
+    def __init__(self, groups ):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('spaceshift','images', 'star.png')).convert_alpha()
+        self.rect = self.image.get_frect(center=(0, 0))
 
 pygame.init()
 
@@ -42,19 +51,17 @@ all_sprites = pygame.sprite.Group()
 player = Player(all_sprites)
 
 
-star_surface = pygame.image.load(join('spaceshift','images', 'star.png')).convert_alpha()
-star_coordinates = []
+stars = []
 for i in range (20):
-    star_coordinates.append((randrange(0, WIDTH), randrange(0, HEIGHT)))
+    star = Star(all_sprites)
+    star.rect.center = (randrange(0, WIDTH), randrange(0, HEIGHT))
+    stars.append(star)
 
 meteor_surface = pygame.image.load(join('spaceshift', 'images', 'meteor.png')).convert_alpha()
-meteor_rectangle = meteor_surface.get_frect(center=(WIDTH / 2, HEIGHT/2))
+meteor_rectangle = meteor_surface.get_frect(center=(WIDTH / 2, HEIGHT / 2))
 
 laser_surface = pygame.image.load(join('spaceshift', 'images', 'laser.png')).convert_alpha()
 laser_rectangle = laser_surface.get_frect(bottomleft=(20, HEIGHT - 20))
-
-
-
 
 
 while not exit:
@@ -66,25 +73,18 @@ while not exit:
     all_sprites.update(delta_time)
     keys = pygame.key.get_pressed()
     
-        
-    #laser
-    k = pygame.key.get_just_pressed()
-    if k[pygame.K_SPACE]:
-        print("space pressed")
-  
-    
-        
+
     display_surface.fill('darkgrey')#if you remove htis line it does not claer
-    for i in star_coordinates:
-        display_surface.blit(star_surface, i)#putting one surface on another surface
-    
+    """ for star in stars:
+        display_surface.blit(star.image, star.rect)#putting one surface on another surface
+     """
     
     #display_surface.blit(player_surf, (surface_x, 5))#putting one surface on another surface
     
       
     display_surface.blit(meteor_surface, meteor_rectangle)    
     display_surface.blit(laser_surface, laser_rectangle)    
-    display_surface.blit(player.image, player.rect)
+    #display_surface.blit(player.image, player.rect)
     all_sprites.draw(display_surface)
     pygame.display.update()
 
